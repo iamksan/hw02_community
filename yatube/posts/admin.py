e5ae46b5091906382286bcaa5baad5b6b96fe21c
Никита Остапенко <nikitaostapenko0968@gmail.com>
2022-11-11 13:11:24 +0500

    6

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -17,4 +17,6 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-admin.site.register(Post, PostAdmin, Group)
+admin.site.register(Post, PostAdmin)
+
+admin.site.register(Group)
